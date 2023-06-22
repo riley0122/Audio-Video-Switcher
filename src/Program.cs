5eb58e17c5ab7ecc190936dc200a67f8ba653bf9
LucasAmmer <lucas@ammer.nl>
2023-06-22 16:45:45 +0200

    added what should do the trick

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace avSwitcher{
+﻿using FFMpegCore;
+
+namespace avSwitcher{
     class Program{
         static void Main(string[] args){
             string filename = args[0];
+            string output = filename + "-inverted.mp4";
+
+            FFMpegArguments
+                .FromFileInput(filename)
+                .OutputToFile(output, false, options => options
+                    .WithCustomArgument("-map 0:a:0 -map 0:v:1"))
+                .ProcessSynchronously();
+
+            Console.WriteLine("switched");
         }
     }
 }
