e77f01abbb5d84f35ce20e5148eaec4978a50fd8
LucasAmmer <lucas@ammer.nl>
2023-06-22 15:56:47 +0200

    added sample file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader};
+use mp4::{Result};
+
+fn main() -> Result<()> {
+    let FILE_LOCATION = "./Sample.mp4";
+
+    let thefile = File::open(FILE_LOCATION);
+
+    Ok(())
 }
