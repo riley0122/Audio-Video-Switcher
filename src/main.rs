8c1b98511f02901d0ef8a609408b86abcc20a7e5
LucasAmmer <lucas@ammer.nl>
2023-06-22 16:10:33 +0200

    ADDED the thing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use mp4::{Result};
 fn main() -> Result<()> {
     let FILE_LOCATION = "./Sample.mp4";
 
-    let thefile = File::open(FILE_LOCATION);
+    let thefile = File::open(FILE_LOCATION).unwrap();
+    let size = thefile.metadata()?.len();
+    let reader = BufReader::new(thefile);
+    let mp4 = mp4::Mp4Reader::read_header(reader, size)?;
 
     Ok(())
 }
